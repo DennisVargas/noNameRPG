1383674bd83a2b5897dcba771915660bc8d9d0e9
Dennis_Vargas <dennis.vargas@wsu.edu>
2017-11-06 21:44:46 -0800

    Creates a state id for basic being test
    With the state id of 20 BasicBeingTestState can be created and added to
    the Project2 main app. This state will be loaded first when project 2
    launches. Entity coarse grained collision boundary is also set to
    Circle in init states list.

--- src/Project2/Project2.java
+++ src/Project2/Project2.java
@@ -1,5 +1,6 @@
 package Project2;
 
+import jig.Entity;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
@@ -10,6 +11,10 @@ public class Project2 extends StateBasedGame {
     public static final int HEIGHT = (WIDTH/16)*9;
     public static final float SCALE = 1f;
     public static final String NAME = "Game Project 2";
+
+    public static final int BASICBEINGTESTSTATE = 20;
+
+
 
     public Project2() {
         super(NAME);
@@ -25,6 +30,9 @@ public class Project2 extends StateBasedGame {
 
     @Override
     public void initStatesList(GameContainer gameContainer) throws SlickException {
+        Entity.setCoarseGrainedCollisionBoundary(Entity.CIRCLE);
+        this.addState(new BasicBeingTestState(BASICBEINGTESTSTATE));
+
     }
 
 }
