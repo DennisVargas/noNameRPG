bb2fa648e84528960cfbc0236a9c6898db20345e
DennisVargas <dennis.vargas@wsu.edu>
2017-11-10 18:59:06 -0800

    Creates MainMenuState.java MainMenuState extends BasicGameState At the moment it reports the State id in the center of screen as a string.

--- src/Project2/MainMenuState.java
+++ src/Project2/MainMenuState.java
@@ -1,4 +1,43 @@
 package Project2;
 
-public class MainMenuState {
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.GameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class MainMenuState extends BasicGameState {
+    private int stateId;
+
+    public MainMenuState(int stateId) {
+        this.stateId = stateId;
+    }
+
+    @Override
+    public int getID() {
+        return stateId;
+    }
+
+    @Override
+    public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
+        System.out.println("Init Main Menu State "
+                + gameContainer.getScreenHeight());
+    }
+
+    @Override
+    public void enter(GameContainer container, StateBasedGame game) throws SlickException {
+        super.enter(container, game);
+        System.out.println("Entering Main Menu state");
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
+        graphics.drawString(String.valueOf(stateId), 640,360);
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
+
+    }
 }
