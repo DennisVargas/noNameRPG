15f6f45e493cabd42b60d874c432378a93e456ac
DennisVargas <dennis.vargas@wsu.edu>
2017-11-10 18:57:18 -0800

    Creates MenuItem.java This is a class which extends JIG Entity and allows for menu items to be selected by swapping from one image to the next in an animation.

--- src/Project2/MenuItem.java
+++ src/Project2/MenuItem.java
@@ -1,4 +1,12 @@
 package Project2;
 
-public class MenuItem {
+import jig.Entity;
+import jig.Vector;
+import org.newdawn.slick.Animation;
+
+public class MenuItem extends Entity {
+    Animation selectionHighlight = new Animation();
+    MenuItem(Vector position, String notSelectedImage, String SelectedImage){
+
+    }
 }
