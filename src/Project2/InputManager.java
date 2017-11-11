8400535d155bf0fda0200d69338e920c4132ae7f
DennisVargas <dennis.vargas@wsu.edu>
2017-11-11 01:52:02 -0800

    Creates an or statment in Input Manager Process Input that checks menus.
    ProcessInput now uses menu input on all menus not just MainMenu.

--- src/Project2/InputManager.java
+++ src/Project2/InputManager.java
@@ -5,7 +5,8 @@ import org.newdawn.slick.Input;
 public class InputManager {
 
     public static String ProcessInput(Input input, int stateId) {
-        if (stateId == Project2.MAINMENUSTATE) {
+        if (stateId == Project2.MAINMENUSTATE || stateId == Project2.OPTIONMENUSTATE
+                || stateId == Project2.NEWSINGLEMENUSTATE|| stateId == Project2.NEWMULTIMENUSTATE) {
             // if input key is down
             if (input.isKeyPressed(Input.KEY_DOWN)) {
                 return "down";
