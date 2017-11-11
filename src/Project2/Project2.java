f826723848dded311f7e32805b022a93b7e7a7bd
DennisVargas <dennis.vargas@wsu.edu>
2017-11-11 01:55:42 -0800

    Adds all possible gamestates to the Project2 main app java class.

--- src/Project2/Project2.java
+++ src/Project2/Project2.java
@@ -19,18 +19,29 @@ public class Project2 extends StateBasedGame {
     public static final float SCALE = 1f;
     public static final String NAME = "Game Project 2";
 
+
+    /**
+     * 5 MAIN GAME STATES
+     */
     public static final int MAINMENUSTATE = 0;
+    public static int NEWSINGLEMENUSTATE = 1;
+    public static final int NEWMULTIMENUSTATE = 2;
+    public static final int OPTIONMENUSTATE = 3;
+    public static final int GAMEPLAYSTATE = 4;
+
     /**
      * these are defined test states for development purposes
      */
     public static final int BASICBEINGTESTSTATE = 20;
     public static final int MENUITEMTESTSTATE = 21;
 
-
-
-
     public Project2() {
         super(NAME);
+        this.addState(new MainMenuState(MAINMENUSTATE));
+        this.addState(new NewSingleMenu(NEWSINGLEMENUSTATE));
+        this.addState(new NewMultiMenu(NEWMULTIMENUSTATE));
+        this.addState(new OptionMenuState(OPTIONMENUSTATE));
+        this.addState(new GamePlayState(GAMEPLAYSTATE));
     }
 
     public static void main(String[] args)throws SlickException{
@@ -48,8 +59,11 @@ public class Project2 extends StateBasedGame {
         Entity.setCoarseGrainedCollisionBoundary(Entity.CIRCLE);
 //        this.addState(new TestStateMenuItem(BASICBEINGTESTSTATE));
 //        this.addState(new TestStateBasicBeing(BASICBEINGTESTSTATE));
-        this.addState(new MainMenuState(MAINMENUSTATE));
-
+        this.getState(MAINMENUSTATE).init(gameContainer, this);
+        this.getState(NEWSINGLEMENUSTATE).init(gameContainer, this);
+        this.getState(NEWMULTIMENUSTATE).init(gameContainer, this);
+        this.getState(OPTIONMENUSTATE).init(gameContainer, this);
+        this.getState(GAMEPLAYSTATE).init(gameContainer, this);
     }
 
 }
