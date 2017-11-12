3a370f7e3527ebf50ed61c20c340a66cde38a955
DennisVargas <dennis.vargas@wsu.edu>
2017-11-11 18:21:16 -0800

    Adds a input poll for the update method of play state. Input is polled and processed. The only input that does anything is the escape key which returns the user to the options menu.

--- src/Project2/GamePlayState.java
+++ src/Project2/GamePlayState.java
@@ -2,6 +2,7 @@ package Project2;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.GameState;
@@ -9,6 +10,7 @@ import org.newdawn.slick.state.StateBasedGame;
 
 public class GamePlayState extends BasicGameState {
     private int stateId;
+    private InputManager.InputCommands inputCommand;
     public GamePlayState(int stateId) {
         this.stateId = stateId;
     }
@@ -30,11 +32,22 @@ public class GamePlayState extends BasicGameState {
 
     @Override
     public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
-        graphics.drawString("This is the gameplay state no way out currently",400f,300);
+        graphics.drawString("This is the gameplay state. Press ESC to go back",400f,300);
     }
 
     @Override
     public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
-
+        Input input = gameContainer.getInput();
+        inputCommand =InputManager.ProcessInput(input, stateId);
+        ProcessInputCommand(inputCommand, stateBasedGame);
     }
+
+    private void ProcessInputCommand(InputManager.InputCommands inputCommand, StateBasedGame stateBasedGame) {
+        switch (inputCommand){
+            case back:
+                stateBasedGame.enterState(Project2.OPTIONMENUSTATE);
+                break;
+        }
+    }
+
 }
