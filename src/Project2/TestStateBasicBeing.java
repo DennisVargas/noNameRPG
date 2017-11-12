88b54bb0f70ddbdaffc9c883fc9b5ec918a7ec54
DennisVargas <dennis.vargas@wsu.edu>
2017-11-11 22:01:37 -0800

    Creates a test state for the basic being. Parts of this will eventually make it into the play state but as for now this works for getting a single character on the screen.

--- src/Project2/TestStateBasicBeing.java
+++ src/Project2/TestStateBasicBeing.java
@@ -4,16 +4,21 @@ import jig.ResourceManager;
 import jig.Vector;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+import static Project2.InputManager.InputCommands;
+import static Project2.InputManager.InputCommands.*;
 
 public class TestStateBasicBeing extends BasicGameState{
-    private int state_id;
+    private int stateId;
     private int n;
     private BasicBeing being1;
+    private InputCommands inputCommand;
+
     public TestStateBasicBeing(int state_id) {
-        this.state_id = state_id;
+        this.stateId = state_id;
     }
 
     private final String WALKINGSHEETRSC = "resources/Characters/CrystalBuddy.png";
@@ -21,7 +26,7 @@ public class TestStateBasicBeing extends BasicGameState{
 
     @Override
     public int getID() {
-        return this.state_id;
+        return this.stateId;
     }
 
     @Override
@@ -33,19 +38,33 @@ public class TestStateBasicBeing extends BasicGameState{
     @Override
     public void enter(GameContainer container, StateBasedGame game) throws SlickException {
         super.enter(container, game);
-        being1 = new BasicBeing(new Vector(200,200), ResourceManager.getSpriteSheet(WALKINGSHEETRSC,32,32),
+        being1 = new BasicBeing(new Vector(1260,680), ResourceManager.getSpriteSheet(WALKINGSHEETRSC,32,32),
                 ResourceManager.getSpriteSheet(ATTACKINGSHEETRSC,32,32));
     }
 
     @Override
     public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
         graphics.drawString("hello Test Basic Being " + n, 640,360);
-        being1.RenderBeing();
+        being1.RenderBeing(graphics);
     }
 
     @Override
     public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
-        n++;
+        Input input = gameContainer.getInput();
+        inputCommand = InputManager.ProcessInput(input, stateId);
+
+        ProcessInputCommand(inputCommand);
+    }
+
+    private void ProcessInputCommand(InputCommands inputCommand) {
+        // other things to do in playstate for input various things will need this input?
+        //  pass the inputCommand to the Being and it will then react to its command.
+        //  they will check their own next move for collision and will set the next move in
+        //  preparation for their update call.
+        being1.GenerateNextMove(inputCommand);
+        //  update the beings position and health inside of
+        //  private methods.
+        being1.UpdateBeing();
     }
 
 }
