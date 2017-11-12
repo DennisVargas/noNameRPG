5e0c36cb849ee46b8ab64932d6b8e03f95076c8f
DennisVargas <dennis.vargas@wsu.edu>
2017-11-11 18:20:22 -0800

    Changes Input Manager in that Menu and Game Processing are subroutines. These are called by the public function ProcessInput(input, stateId). After stateId is evaluated teh input is passed on to other methods for evaluation. An enum called InputCommand gives values for up, down, left, right, enter, idle, attack, back.

--- src/Project2/InputManager.java
+++ src/Project2/InputManager.java
@@ -4,38 +4,58 @@ import org.newdawn.slick.Input;
 
 public class InputManager {
 
+    public enum InputCommands {up, down, left, right, enter, idle, attack, back}
+
     private static boolean buttonBeenPressed;
 
-    public static String ProcessInput(Input input, int stateId) {
-
+    public static InputCommands ProcessInput(Input input, int stateId) {
+//      if the stateId is equal to any of the menu states then ProcessMenuInput
         if (stateId == Project2.MAINMENUSTATE || stateId == Project2.OPTIONMENUSTATE
                 || stateId == Project2.NEWSINGLEMENUSTATE|| stateId == Project2.NEWMULTIMENUSTATE) {
-            // if input key is down
-            if (input.isKeyPressed(Input.KEY_DOWN)) {
-                return "down";
-            } else if (input.isKeyPressed(Input.KEY_UP)) {
-                return "up";
-            } else if (input.isKeyPressed(Input.KEY_ENTER)) {
-                return "enter";
+            return ProcessMenuInput(input);
+        }
+//      if currently in the game state
+        else if(stateId == Project2.GAMEPLAYSTATE){
+            return ProcessGamePlayInput(input);
+        }
+        else
+            return InputCommands.idle;
+    }
+
+    private static InputManager.InputCommands ProcessGamePlayInput(Input input) {
+        if(input.isKeyPressed(Input.KEY_ESCAPE)){
+            return InputCommands.back;
+        }
+        return InputCommands.idle;
+    }
+
+    private static InputCommands ProcessMenuInput(Input input){
+        // if input key is down
+        if (input.isKeyPressed(Input.KEY_DOWN)) {
+            return InputCommands.down;
+        } else if (input.isKeyPressed(Input.KEY_UP)) {
+            return InputCommands.up;
+        } else if (input.isKeyPressed(Input.KEY_ENTER)) {
+            return InputCommands.enter;
+        }
+        if (input.isButton1Pressed(0)) {
+            if(!buttonBeenPressed){
+                buttonBeenPressed = true;
+                return InputCommands.enter;
             }
+        } else if (input.isControllerUp(0)) {
+            if(!buttonBeenPressed){
+                buttonBeenPressed = true;
+                return InputCommands.up;}
 
-            if (input.isButton1Pressed(0)) {
-                if(!buttonBeenPressed){
-                    buttonBeenPressed = true;
-                    return "enter";}
-            } else if (input.isControllerUp(0)) {
-                if(!buttonBeenPressed){
-                    buttonBeenPressed = true;
-                    return "up";}
-
-            } else if (input.isControllerDown(0)) {
-                if(!buttonBeenPressed){
-                    buttonBeenPressed = true;
-                    return "down";}
-            }else
-                buttonBeenPressed = false;
-            return "none";
+        } else if (input.isControllerDown(0)) {
+            if(!buttonBeenPressed){
+                buttonBeenPressed = true;
+                return InputCommands.down;}
         }else
-            return "none";
+            buttonBeenPressed = false;
+        return InputCommands.idle;
     }
 }
+
+
