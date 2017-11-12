b54072b79385edbc923dac32fb6fd5b4d086944d
DennisVargas <dennis.vargas@wsu.edu>
2017-11-11 18:24:53 -0800

    Creates variables and methods to Basic Being for Initializiation and rendering. Basic Being constructor thats as argument Vector, and 2 sprite sheets. The vector is initialized by the super class while the sprite sheets are used to initialize the animations on the basic being for walking and attacking. Render Being method renders the being depending on the next Move command.

--- src/Project2/BasicBeing.java
+++ src/Project2/BasicBeing.java
@@ -1,4 +1,47 @@
 package Project2;
 
-public class BasicBeing {
+import jig.Entity;
+import jig.Vector;
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.SpriteSheet;
+
+/**
+ * The BasicBeing class will define all HeroTypes and MobTypes
+ * this class defines all attributes heroes and enemies.
+ * the biggest distinction between the two is controller type
+ * HeroTypes will never be controlled by AI according to spec.
+ */
+public class BasicBeing extends Entity{
+    private float health = 1f;
+    Animation walkRightAnim, walkLeftAnim;
+    String name = "default";
+    private String nextMoveCommand;
+
+    public BasicBeing(Vector position, SpriteSheet walkingSheet, SpriteSheet attackingSheet) {
+        super(position);
+        InitAnimations(walkingSheet, attackingSheet);
+
+    }
+    public BasicBeing(){
+
+    }
+
+    private void InitAnimations(SpriteSheet walkingSheet, SpriteSheet attackingSheet) {
+        this.walkRightAnim = new Animation(walkingSheet, 0,0,5,0,true,100,true);
+        this.walkLeftAnim = new Animation(attackingSheet, 1,2,6,1,true,100,true);
+    }
+
+    public void RenderBeing(){
+        if(nextMoveCommand == "walk-right"){
+            walkRightAnim.draw( getPosition().getX(),
+                                getPosition().getY());}
+        else if(nextMoveCommand == "walk-left"){
+            walkLeftAnim.draw( getPosition().getX(),
+                               getPosition().getY());
+        }
+    }
+
+
+
+
 }
