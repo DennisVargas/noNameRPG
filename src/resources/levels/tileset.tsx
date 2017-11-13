5ec55e69882516561e1b47b4117a16e431222b0e
Tyler Petty <tylerpetty@live.com>
2017-11-13 02:36:12 -0800

    Previous commit missing files

---
+++ src/resources/levels/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset" tilewidth="32" tileheight="32" tilecount="105" columns="21">
+ <image source="dungeon_tiles_compact_and_varied.png" width="672" height="160"/>
+</tileset>
