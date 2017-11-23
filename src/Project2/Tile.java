b84be89c6edc377e93ca6be920ea1d3d1a4004b3
Tyler Petty <tylerpetty@live.com>
2017-11-22 18:16:14 -0800

    reworked level 1 so main layer has walls and door layer has open doorways

--- src/Project2/Tile.java
+++ src/Project2/Tile.java
@@ -1,4 +1,24 @@
 package Project2;
 
 public class Tile {
+    int minX = 0;
+    int maxX = 0;
+    int minY = 0;
+    int maxY = 0;
+    int centerX = 0;
+    int centerY = 0;
+    boolean blocked;
+
+    Tile(int minx, int maxx, int miny, int maxy, int centerx, int centery, boolean blocked){
+        this.minX = minx;
+        this.maxX = maxx;
+        this.minY = miny;
+        this.maxY = maxy;
+        this.centerX = centerx;
+        this.centerY = centery;
+        if (blocked == true){
+            //TODO Create collision box
+        }
+        this.blocked = blocked;
+    }
 }
