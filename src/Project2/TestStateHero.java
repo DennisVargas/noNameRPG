8d2217ca860a7264e9d2bb70aee73bf408cd2eb2
DennisVargas <dennis.vargas@wsu.edu>
2017-11-23 02:28:06 -0800

    Creates a fresh test state for Hero class.

---
+++ src/Project2/TestStateHero.java
@@ -0,0 +1,42 @@
+package Project2;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class TestStateHero extends BasicGameState {
+
+    private int stateID;
+    private Hero hero1;
+
+    public TestStateHero(int stateID) {
+        this.stateID = stateID;
+    }
+
+    @Override
+    public int getID() {
+        return stateID;
+    }
+
+    @Override
+    public void init(GameContainer container, StateBasedGame game) throws SlickException {
+
+    }
+
+    @Override
+    public void enter(GameContainer container, StateBasedGame game) throws SlickException {
+        super.enter(container, game);
+    }
+
+    @Override
+    public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
+
+    }
+}
