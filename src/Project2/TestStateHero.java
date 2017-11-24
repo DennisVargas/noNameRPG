774ff5e450562f60734fc22d1e7cdb4353bc6ff7
DennisVargas <dennis.vargas@wsu.edu>
2017-11-23 18:27:11 -0800

    Creates an ArrayList of beings to iterate upon in TestStateHero.java Using the MoveCalc class calculating the translation and passing and storing this result in the being for collision calculation.

--- src/Project2/TestStateHero.java
+++ src/Project2/TestStateHero.java
@@ -3,14 +3,22 @@ package Project2;
 import jig.Vector;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+
+import java.util.ArrayList;
+
+import static Project2.InputManager.ProcessInput;
+import static Project2.MovementCalc.*;
+import static Project2.InputManager.InputCommands;
 
 public class TestStateHero extends BasicGameState {
 
     private int stateID;
     private Hero hero1;
+    private ArrayList<BasicBeing> beingList ;
 
     public TestStateHero(int stateID) {
         this.stateID = stateID;
@@ -24,6 +32,9 @@ public class TestStateHero extends BasicGameState {
     @Override
     public void init(GameContainer container, StateBasedGame game) throws SlickException {
         hero1 = new Hero(new Vector(0,0), false);
+        beingList = new ArrayList<BasicBeing>();
+        beingList.add(hero1);
+
     }
 
     @Override
@@ -38,6 +49,20 @@ public class TestStateHero extends BasicGameState {
 
     @Override
     public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
+        Input input = container.getInput();
+        InputCommands command = ProcessInput(input, getID());
+        UpdateBeings(beingList, command);
+    }
 
+    private void UpdateBeings(ArrayList<BasicBeing> beings, InputCommands command) {
+//        set the translation for each being.
+
+        for(BasicBeing being: beings) {
+            System.out.println(being.getClass());
+            Vector translation = CalcTranslation(CalcDirection(command), being.getSpeed());
+//        a being's previous translation can be used to move them back from where they came if need be.
+            being.setTranslation(translation);
+        }
+//        check the beings collision
     }
 }
