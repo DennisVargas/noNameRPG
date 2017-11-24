d8db15ea949256e1e71435614ebb3617b505868b
DennisVargas <dennis.vargas@wsu.edu>
2017-11-24 00:02:55 -0800

    Creates Collision Manager Class
    Collision manager class will handle all collision adjustments.

--- src/Project2/CollisionManager.java
+++ src/Project2/CollisionManager.java
@@ -1,4 +1,15 @@
 package Project2;
 
+import java.util.ArrayList;
+
 public class CollisionManager {
+
+    public static void CheckCollisions(ArrayList<BasicBeing>beings){
+        CheckBeingBeingCollisions(beings);
+    }
+
+    private static void CheckBeingBeingCollisions(ArrayList<BasicBeing> beings) {
+
+    }
+
 }
