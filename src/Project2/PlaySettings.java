152fa92eb814ce25933997d67918988f7c9fddfe
DennisVargas <dennis.vargas@wsu.edu>
2017-11-25 02:38:14 -0800

    Creates PlaySettings.java
    Holds a field ipAddress that is a string set in the constructor of PlaySettings.

---
+++ src/Project2/PlaySettings.java
@@ -0,0 +1,17 @@
+package Project2;
+
+public class PlaySettings {
+    private String ipAddress;
+
+    public PlaySettings(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+}
