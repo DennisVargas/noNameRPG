6c78d74c00185a0f10a29b5035b797d942fff1a1
DennisVargas <dennis.vargas@wsu.edu>
2017-11-25 19:35:25 -0800

    Collisions are detected by CheckBeingBeingCollisions.
    No effect is applied to being health if collision is hero * mob.

--- src/Project2/CollisionManager.java
+++ src/Project2/CollisionManager.java
@@ -1,5 +1,7 @@
 package Project2;
 
+import jig.Vector;
+
 import java.util.ArrayList;
 
 public class CollisionManager {
@@ -8,8 +10,24 @@ public class CollisionManager {
         CheckBeingBeingCollisions(beings);
     }
 
-    private static void CheckBeingBeingCollisions(ArrayList<BasicBeing> beings) {
-
+    public static void CheckBeingBeingCollisions(ArrayList<BasicBeing> beings) {
+//        as beings are tested move
+        ArrayList <BasicBeing> testList;
+        for(BasicBeing being: beings){
+            testList = (ArrayList<BasicBeing>)beings.clone();
+            testList.remove(being);
+            for(BasicBeing testBeing: testList){
+                if(being.collides(testBeing)!= null){
+                    System.out.println("collision between "+being.getName()+" and "+ testBeing.getName());
+                    float undoX, undoY;
+                    undoX = being.getTranslation().getX()*-1f;
+                    undoY = being.getTranslation().getY()*-1f;
+                    Vector undoMoveTranslation = new Vector(undoX, undoY);
+                    being.setTranslation(undoMoveTranslation);
+                    being.setWorldPosition(MovementCalc.CalcWorldPosition(undoMoveTranslation,being.getWorldPosition()));
+                }
+            }
+        }
     }
 
 }
