4f6bf176b86acd1c35bdec585109060882bae0d4
DennisVargas <dennis.vargas@wsu.edu>
2017-11-26 19:14:04 -0800

    Checks if the difference between the position of two beings is less than 1
    In the case that the difference is less than one a collision has occured and movement will be triggered in the opposite direction.

--- src/Project2/CollisionManager.java
+++ src/Project2/CollisionManager.java
@@ -16,22 +16,33 @@ public class CollisionManager {
             return CheckBeingBeingCollisions(being, beings);
     }
 
-    public static void CheckBeingBeingCollisions(ArrayList<BasicBeing> beings) {
-//        as beings are tested move
+    public static boolean CheckBeingBeingCollisions(BasicBeing being, ArrayList<BasicBeing>beings) {
         ArrayList <BasicBeing> testList;
-        for(BasicBeing being: beings){
-            testList = (ArrayList<BasicBeing>)beings.clone();
-            testList.remove(being);
-            for(BasicBeing testBeing: testList){
-                if(being.collides(testBeing)!= null){
-                    System.out.println("collision between "+being.getName()+" and "+ testBeing.getName());
-                    float undoX, undoY;
-                    undoX = being.getTranslation().getX()*-1f;
-                    undoY = being.getTranslation().getY()*-1f;
-                    Vector undoMoveTranslation = new Vector(undoX, undoY);
-                    being.setTranslation(undoMoveTranslation);
-                    being.setWorldPosition(MovementCalc.CalcWorldPosition(undoMoveTranslation,being.getWorldPosition()));
+        testList = (ArrayList<BasicBeing>)beings.clone();
+        testList.remove(being);
+        for(BasicBeing testBeing: testList){
+//          get the difference between the x coords
+            float diffX = being.getWorldPositionX() - testBeing.getWorldPositionX();
+//          get the difference between the y coords
+            float diffY = being.getWorldPositionY() - testBeing.getWorldPositionY();
+
+//            if differences are less than one you've collided
+            if(abs(diffX) < 1 && abs(diffY) < 1){
+                System.out.println("XY-Collide");
+//              Reverse the being's last move by reversing the move command and creating a new world position vector
+//              alternatively we could store the being previous location
+                being.setWorldPosition(MovementCalc.CalcWorldPosition(
+                        MovementCalc.CalcTranslation(
+                                MovementCalc.CalcDirection(
+                                        ReverseCommand(being.getCommand()))
+                                ,being.getSpeed())
+                        ,being.getWorldPosition()));
+//                the being is not a mob
+                if(!being.getName().contains("mob")){
+                    if(being.getCommand() == InputCommands.attack){
+                    }
                 }
+                return true;
             }
         }
     }
