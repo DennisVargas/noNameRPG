05dc39d1f1eb9e2406317a3a6c0c4ded405dce28
DennisVargas <dennis.vargas@wsu.edu>
2017-11-26 19:16:16 -0800

    Changes hero constructor method for setting npc JIG position
    Jig position is calculated using the player map position not the middle of the screen

--- src/Project2/Hero.java
+++ src/Project2/Hero.java
@@ -33,10 +33,11 @@ public class Hero extends BasicBeing{
             this.InitAnimations(rangedWalkingSheet,rangedAttackingSheet);
         }
 //        if the hero isn't a client then don't place them in center screen
-//        todo: when the world class is created change this to a comparison of the ip address and a name argument passed into the Hero constructor
         if(this.getName() != Project2.settings.getIpAddress())
             this.setPosition(MovementCalc.CalcScreenPosition(
-                    new Vector((Project2.WIDTH * Project2.SCALE)/2f,(Project2.HEIGHT * Project2.SCALE)/2f),
+                    Project2.getSettings().getPlayer().getWorldPosition(),
                     this.getWorldPosition()));
+        else
+            Project2.getSettings().setPlayer(this);
     }
 }
