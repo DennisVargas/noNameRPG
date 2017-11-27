bbb7f4155ee61c9659d733dadcb8abb8378b4a5e
Amy Phillips <amy.phillips@wsu.edu>
2017-11-26 20:09:11 -0800

    Missed file in previous commit.

---
+++ src/Project2/MobList.java
@@ -0,0 +1,35 @@
+package Project2;
+
+
+import jig.Vector;
+
+import java.util.ArrayList;
+
+public class MobList {
+
+    private ArrayList<Mob> Mobs;
+
+    public MobList() {
+        Mobs = new ArrayList<Mob>();
+    }
+
+    public ArrayList getMobList (int level) {
+        Mob mob1, mob2, mob3, mob4, mob5, mob6;
+        if (level == 1) {
+            mob1 = new Mob(new Vector(90f, 104.5f), 1, "mob1");
+            Mobs.add(mob1);
+
+            mob1 = new Mob(new Vector(81f, 104f), 1, "mob1");
+            mob2 = new Mob(new Vector(80.5f, 104.5f), 1, "mob2");
+            mob3 = new Mob(new Vector(81.6f, 104.3f), 1, "mob3");
+            mob4 = new Mob(new Vector(81.1f, 105f), 1, "mob4");
+            mob4 = new Mob(new Vector(81.1f, 105f), 1, "mob5");
+            mob4 = new Mob(new Vector(81.1f, 105f), 1, "mob6");
+            Mobs.add(mob1);
+            Mobs.add(mob2);
+            Mobs.add(mob3);
+            Mobs.add(mob4);
+        }
+        return Mobs;
+    }
+}
