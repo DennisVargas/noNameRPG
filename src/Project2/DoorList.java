0f9df905f7ea0ac1c0a5cb5ddd23b8c8c9ffc990
Tyler Petty <tylerpetty@live.com>
2017-12-02 20:28:17 -0800

    Changed door locations and added keys to map

--- src/Project2/DoorList.java
+++ src/Project2/DoorList.java
@@ -24,28 +24,28 @@ public class DoorList {
             Doors.add(door);
             door = new Door(new Vector(26.5f, 79.25f), doorHName); doorNum++; doorHName = "doorH" + doorNum;
             Doors.add(door);
-            door = new Door(new Vector(28.5f, 99.25f), doorVName); doorNum++; doorVName = "doorV" + doorNum;
-            Doors.add(door);
+//            door = new Door(new Vector(28.5f, 99.25f), doorVName); doorNum++; doorVName = "doorV" + doorNum;
+//            Doors.add(door);
 
 //          Room 3
-            door = new Door(new Vector(30.5f, 69.25f), doorVName); doorNum++; doorVName = "doorV" + doorNum;
-            Doors.add(door);
+//            door = new Door(new Vector(30.5f, 69.25f), doorVName); doorNum++; doorVName = "doorV" + doorNum;
+//            Doors.add(door);
             door = new Door(new Vector(40.5f, 69.25f), doorVName); doorNum++; doorVName = "doorV" + doorNum;
             Doors.add(door);
             door = new Door(new Vector(38.5f, 61.25f), doorHName); doorNum++; doorHName = "doorH" + doorNum;
             Doors.add(door);
-            door = new Door(new Vector(41.5f, 87.25f), doorVName); doorNum++; doorVName = "doorV" + doorNum;
-            Doors.add(door);
+//            door = new Door(new Vector(41.5f, 87.25f), doorVName); doorNum++; doorVName = "doorV" + doorNum;
+//            Doors.add(door);
 
 //          Room 7
-            door = new Door(new Vector(52.5f, 99.25f), doorVName); doorNum++; doorVName = "doorV" + doorNum;
-            Doors.add(door);
+//            door = new Door(new Vector(52.5f, 99.25f), doorVName); doorNum++; doorVName = "doorV" + doorNum;
+//            Doors.add(door);
 
 //          Room 5
-            door = new Door(new Vector(62.5f, 89.25f), doorHName); doorNum++; doorHName = "doorH" + doorNum;
-            Doors.add(door);
-            door = new Door(new Vector(68.5f, 73.25f), doorHName); doorNum++; doorHName = "doorH" + doorNum;
-            Doors.add(door);
+//            door = new Door(new Vector(62.5f, 89.25f), doorHName); doorNum++; doorHName = "doorH" + doorNum;
+//            Doors.add(door);
+//            door = new Door(new Vector(68.5f, 73.25f), doorHName); doorNum++; doorHName = "doorH" + doorNum;
+//            Doors.add(door);
 
 //            Room 8
             door = new Door(new Vector(78.5f, 75.25f), doorVName); doorNum++; doorVName = "doorV" + doorNum;
@@ -54,26 +54,26 @@ public class DoorList {
             Doors.add(door);
             door = new Door(new Vector(80.5f, 67.25f), doorHName); doorNum++; doorHName = "doorH" + doorNum;
             Doors.add(door);
-            door = new Door(new Vector(86.5f, 77.25f), doorHName); doorNum++; doorHName = "doorH" + doorNum;
-            Doors.add(door);
+//            door = new Door(new Vector(86.5f, 77.25f), doorHName); doorNum++; doorHName = "doorH" + doorNum;
+//            Doors.add(door);
 
 //            Room 6
-            door = new Door(new Vector(84.5f, 51.25f), doorVName); doorNum++; doorVName = "doorV" + doorNum;
-            Doors.add(door);
-            door = new Door(new Vector(94.5f, 39.25f), doorVName); doorNum++; doorVName = "doorV" + doorNum;
-            Doors.add(door);
+//            door = new Door(new Vector(84.5f, 51.25f), doorVName); doorNum++; doorVName = "doorV" + doorNum;
+//            Doors.add(door);
+//            door = new Door(new Vector(94.5f, 39.25f), doorVName); doorNum++; doorVName = "doorV" + doorNum;
+//            Doors.add(door);
 
 //            Room 4
-            door = new Door(new Vector(62.5f, 37.25f), doorHName); doorNum++; doorHName = "doorH" + doorNum;
-            Doors.add(door);
+//            door = new Door(new Vector(62.5f, 37.25f), doorHName); doorNum++; doorHName = "doorH" + doorNum;
+//            Doors.add(door);
             door = new Door(new Vector(76.5f, 39.25f), doorVName); doorNum++; doorVName = "doorV" + doorNum;
             Doors.add(door);
             door = new Door(new Vector(74.5f, 59.25f), doorHName); doorNum++; doorHName = "doorH" + doorNum;
             Doors.add(door);
 
 //            Room 2
-            door = new Door(new Vector(18.5f, 33.25f), doorVName); doorNum++; doorVName = "doorV" + doorNum;
-            Doors.add(door);
+//            door = new Door(new Vector(18.5f, 33.25f), doorVName); doorNum++; doorVName = "doorV" + doorNum;
+//            Doors.add(door);
             door = new Door(new Vector(52.5f, 27.25f), doorVName); doorNum++; doorVName = "doorV" + doorNum;
             Doors.add(door);
             door = new Door(new Vector(32.5f, 47.25f), doorHName); doorNum++; doorHName = "doorH" + doorNum;
--- src/Project2/KeyList.java
+++ src/Project2/KeyList.java
@@ -18,12 +18,22 @@ public class KeyList {
         String keyName = "key" + keyNum;
         if (level == 1) {
             // Room 1
-            key = new Key(new Vector(30.5f, 105.25f), keyName, 1); keyNum++; keyName = "key" + keyNum;
+            key = new Key(new Vector(62f, 105f), keyName, 1); keyNum++; keyName = "key" + keyNum;
             Keys.add(key);
-            key = new Key(new Vector(35.5f, 105.25f), keyName, 1); keyNum++; keyName = "key" + keyNum;
+            key = new Key(new Vector(33f, 93f), keyName, 1); keyNum++; keyName = "key" + keyNum;
             Keys.add(key);
-
-
+            key = new Key(new Vector(26f, 63f), keyName, 1); keyNum++; keyName = "key" + keyNum;
+            Keys.add(key);
+            key = new Key(new Vector(50f, 69f), keyName, 1); keyNum++; keyName = "key" + keyNum;
+            Keys.add(key);
+            key = new Key(new Vector(63f, 69f), keyName, 1); keyNum++; keyName = "key" + keyNum;
+            Keys.add(key);
+            key = new Key(new Vector(92f, 81f), keyName, 1); keyNum++; keyName = "key" + keyNum;
+            Keys.add(key);
+            key = new Key(new Vector(92f, 33f), keyName, 1); keyNum++; keyName = "key" + keyNum;
+            Keys.add(key);
+            key = new Key(new Vector(74f, 33f), keyName, 1); keyNum++; keyName = "key" + keyNum;
+            Keys.add(key);
         }
         return this.Keys;
     }
